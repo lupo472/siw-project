a7a729b53718fcfbaa4c4ead9890ea9d6307baff
Luca Antilici <luc.antilici@stud.uniroma3.it>
2015-06-04 17:09:17 +0200

    Tolto @Entity da User

--- src/it/uniroma3/siw/model/User.java
+++ src/it/uniroma3/siw/model/User.java
@@ -13,7 +13,7 @@ import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-@Entity
+
 public abstract class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
